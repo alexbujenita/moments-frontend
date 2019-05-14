ff91503ed4066080c4edad7fda854431c4f66703
Alex Bujenita <dekc89@gmail.com>
2019-05-14 16:21:34 +0100

    secrets test

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import { Route, Switch, withRouter } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage';
 import PageNotFound from './components/My404/My404';
+
+import aws from './secrets'
 import './App.css';
 
 class App extends Component {
 
 
   render() {
-    console.log(process.env.REACT_APP_AWS_CONFIG);
-    
+    console.log(aws())
     return (
       <Switch>
         <Route path='/' exact component={LandingPage} />
