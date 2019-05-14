1fc8e0037d3edc4a4ac6b7357d4040332ea67c90
Alex Bujenita <dekc89@gmail.com>
2019-05-14 16:08:12 +0100

    Building Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Switch, withRouter } from "react-router-dom";
+import LandingPage from './components/LandingPage/LandingPage';
+import PageNotFound from './components/My404/My404';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
 
+
+  render() {
+    return (
+      <Switch>
+        <Route path='/' exact component={LandingPage} />
+        <Route path='*' exact component={PageNotFound} />
+      </Switch>
+    )
+  }
+}
 export default App;
---
+++ src/components/My404/My404.js
@@ -0,0 +1,10 @@
+import React from "react"
+import notFound from './hero-blog-404.jpg'
+
+const PageNotFound = () => {
+  return (
+    <img src={notFound} alt="404 not found" />
+  )
+}
+
+export default PageNotFound
