1b9ab2b082dd81129034919460059cdb3c09a88a
Alex Bujenita <dekc89@gmail.com>
2019-05-16 15:49:26 +0100

    basic Discover and PhotCa skeleton done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import PageNotFound from './components/My404/My404';
 
 import aws from './secrets'
 import './App.css';
+import Discover from './components/Discover/Discover';
 
 class App extends Component {
 
@@ -17,6 +18,7 @@ class App extends Component {
         <Route path='/' exact component={LandingPage} />
         <Route path='/test' exact render={() =>  <h1 onClick={()=>this.props.history.push('/')}>TEST</h1>} />
         <Route path='/authorize' exact component={Authorize} />
+        <Route path='/discover' exact component={Discover} />
         <Route path='*' exact component={PageNotFound} />
       </Switch>
     )
