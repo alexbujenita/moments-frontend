e69f5bba92b8522682213914f3177696a330379f
Alex Bujenita <dekc89@gmail.com>
2019-05-16 17:48:34 +0100

    starting profile page

---
+++ src/components/PhotographerProfile/PhotographerProfile.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class PhotographerProfile extends Component {
+
+  
+
+  render() {
+    console.log(this.props);
+
+    return (
+      <h1>Profile</h1>
+    )
+  }
+}
+
+export default PhotographerProfile
