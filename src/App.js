ea0d741735e03bcf00bbfa4cf6bf0109a8f4daab
Alex Bujenita <dekc89@gmail.com>
2019-05-19 12:31:45 +0100

    about component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import { Route, Switch, withRouter } from "react-router-dom";
 import LandingPage from './components/LandingPage/LandingPage';
 import Authorize from './components/Authorize/Authorize';
 import PageNotFound from './components/My404/My404';
+import About from './components/About/About';
+import Discover from './components/Discover/Discover';
+import PhotographerProfile from './components/PhotographerProfile/PhotographerProfile';
 
 import aws from './secrets'
 import './App.css';
-import Discover from './components/Discover/Discover';
-import PhotographerProfile from './components/PhotographerProfile/PhotographerProfile';
 
 class App extends Component {
 
@@ -17,6 +18,7 @@ class App extends Component {
     return (
       <Switch>
         <Route path='/' exact component={LandingPage} />
+        <Route path='/about' exact component={About} />
         <Route path='/test' exact render={() =>  <h1 onClick={()=>this.props.history.push('/')}>TEST</h1>} />
         <Route path='/authorize' exact component={Authorize} />
         <Route path='/discover' exact component={Discover} />
---
+++ src/components/About/About.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="about">
+      <div>
+        <p>
+          Moments is a place where photographers can meet potential clients.
+        </p>
+        <p>
+          And where people can find the right person to immortalize their most important moments.
+        </p>
+      </div>
+      <div>
+        <p>
+
+        </p>
+      </div>
+      <footer>
+        <p>
+          Built by Alex.
+        </p>
+      </footer>
+    </div>
+  )
+}
+
+export default About;
