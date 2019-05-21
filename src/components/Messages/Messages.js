57051d0a72bab62da70f819161e12d5f7501253b
Alex Bujenita <dekc89@gmail.com>
2019-05-21 13:26:41 +0100

    user has box for messages and can hide seen ones

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import Message from '../Message/Message';
 
+import './Messages.css'
+
 class Messages extends Component {
+
+  state = {
+    hidden: true
+  }
+
+  handleClick = event => {
+    this.setState({
+      hidden: !this.state.hidden
+    })
+  }
 
   render() {
     const { markAsSeen } = this.props
     return (
       <div className="message-container">
         {
-          this.props.messages.map(message => <Message key={message.id} message={message} markAsSeen={markAsSeen} /> )
+          this.props.messages.map(message => <Message
+                      hidden={this.state.hidden}
+                      key={message.id}
+                      message={message}
+                      markAsSeen={markAsSeen} /> )
         }
+        <div>
+          <h3>Personal messages</h3>
+          <span>Hide seen?</span><input  type='checkbox' checked={this.state.hidden} onChange={this.handleClick} />
+        </div>
       </div>
     )
   }
